d64a2be732f05c7ac529e27d91aac5c963b2d8c3
Mamun Sabuj <sabuj@skylarksoft.com>
2023-05-07 16:49:55 +0600

    user id geting from hexadata bugfix

--- zkteco/zklib/zkattendance.php
+++ zkteco/zklib/zkattendance.php
@@ -79,7 +79,9 @@
                 $u1 = hexdec( substr($u[1], 4, 2) );
                 $u2 = hexdec( substr($u[1], 6, 2) );
                 $uid = $u1+($u2*256);
-                $id = intval( str_replace("\0", '', hex2bin( substr($u[1], 6, 14) ) ) );
+                // $id = intval( str_replace("\0", '', hex2bin( substr($u[1], 6, 14) ) ) );
+                $id = intval( substr( str_replace("\0", '', hex2bin( substr($u[1], 6, 20) ) ), 1) );
+
                 $state = hexdec( substr( $u[1], 56, 2 ) );
                 $timestamp = decode_time( hexdec( reverseHex( substr($u[1], 58, 8) ) ) ); 
                 
